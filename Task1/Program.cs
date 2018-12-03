f00e65e43ddbb0ef9acedc96b2fdb3d27f1c6bd5
msmitko <44364545+msmitko@users.noreply.github.com>
2018-12-03 10:45:29 +0200

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void ReturnName(string name)
+    {
+        Console.WriteLine("Hello {0}", name);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter name: ");
+        ReturnName(Console.ReadLine());
+    }
+}
+
---
+++ Task7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static string Reverse(string number)
+    {
+        char[] charArray = number.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        string number = Console.ReadLine();
+
+        Console.WriteLine(Reverse(number));
+    }
+}
+
